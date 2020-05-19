b00b66e175f3e6a83b97d07388b5932a8bfbf7b1
yurisasaki <yurisasaki@localhost>
2020-05-20 03:29:55 +0900

    comment

---
+++ 1-1/src/Food.java
@@ -0,0 +1,12 @@
+
+public class Food {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 String food="お好み焼き";
+		  String reason="いろいろな食材を一品で食べられるから。";
+		  System.out.println("好きな食べ物: "+food);
+		  System.out.println("理由: "+reason);
+	}
+
+}
